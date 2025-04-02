74dee54e39c01d2c50e032c608a11401cc81fe0c
ripudaman <ripudaman@ripudamans-MacBook-Pro.local>
2025-04-01 20:48:51 -0700

    initial commit

---
+++ app/src/test/java/org/example/test1.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class test1 {
+    @Test
+    public void testAppHasGreeting() {
+        App classUnderTest = new App();
+        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    }
+    
+    @Test
+    public void testRandom() {
+        assertEquals(0, 0);
+        assertEquals(1, 1);
+    }
+}
